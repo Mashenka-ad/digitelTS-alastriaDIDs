f5c1d1768670b604a74db745a4cadc2055a1313c
Mashenka-ad <mashenka_ad@protonmail.com>
2021-06-29 14:25:45 +0200

    Issuer, Provider and Holder coded, not minted

---
+++ digitelEntities/1.createEntityProviderDigitelAlastriaID.js
@@ -0,0 +1,158 @@
+const {
+  transactionFactory,
+  UserIdentity,
+  config,
+  tokensFactory
+} = require('alastria-identity-lib')
+const fs = require('fs')
+const Web3 = require('web3')
+const keythereum = require('keythereum')
+
+const rawdata = fs.readFileSync('../configuration.json')
+const configData = JSON.parse(rawdata)
+
+//Nuestro keystore entityDigitelIssuer
+const keyDataEntityProvider = fs.readFileSync(
+  '../keystores/providerData.json'
+)
+const keystoreDataEntityProvider = JSON.parse(keyDataEntityProvider)
+const keyDataAdmin = fs.readFileSync(
+  '../keystores/admin-6e3976aeaa3a59e4af51783cc46ee0ffabc5dc11.json'
+)
+const keystoreDataAdmin = JSON.parse(keyDataAdmin)
+
+// Init your blockchain provider --- > Red T
+const myBlockchainServiceIp = configData.nodeURL
+const web3 = new Web3(new Web3.providers.HttpProvider(myBlockchainServiceIp))
+
+console.log(
+  '\n ------ Example of prepare Alastria ID, addKey and createAlastriaID necessary to have an Alastria ID ------ \n'
+)
+// Data
+
+const adminKeyStore = keystoreDataAdmin
+
+let adminPrivateKey
+try {
+  adminPrivateKey = keythereum.recover(
+    configData.addressPassword,
+    adminKeyStore
+  )
+} catch (error) {
+  console.log('ERROR: ', error)
+  process.exit(1)
+}
+
+const adminIdentity = new UserIdentity(
+  web3,
+  `0x${adminKeyStore.address}`,
+  adminPrivateKey
+)
+
+const entityProviderKeystore = keystoreDataEntityProvider
+
+let entityProviderPrivateKey
+try {
+  entityProviderPrivateKey = keythereum.recover(
+    configData.addressProviderPassword,
+    entityProviderKeystore
+  )
+} catch (error) {
+  console.log('ERROR: ', error)
+  process.exit(1)
+}
+
+const entityProviderIdentity = new UserIdentity(
+  web3,
+  `0x${entityProviderKeystore.address}`,
+  entityProviderPrivateKey
+)
+// End data
+
+function preparedAlastriaId() {
+  const preparedId = transactionFactory.identityManager.prepareAlastriaID(
+    web3,
+    entityProviderKeystore.address
+  )
+  return preparedId
+}
+
+function createAlastriaId() {
+  const txCreateAlastriaID = transactionFactory.identityManager.createAlastriaIdentity(
+    web3,
+    configData.entityProviderPubk.substr(2)
+  )
+  return txCreateAlastriaID
+}
+
+console.log(
+  '\n ------ A promise all where prepareAlastriaID and createAlsatriaID transactions are signed and sent ------ \n'
+)
+async function main() {
+  const prepareResult = await preparedAlastriaId()
+  const createResult = await createAlastriaId()
+
+  const signedPreparedTransaction = await adminIdentity.getKnownTransaction(
+    prepareResult
+  )
+  const signedCreateTransaction = await entityIssuerIdentity.getKnownTransaction(
+    createResult
+  )
+  web3.eth
+    .sendSignedTransaction(signedPreparedTransaction)
+    .on('transactionHash', function (hash) {
+      console.log('HASH: ', hash)
+    })
+    .on('receipt', function (receipt) {
+      console.log('RECEIPT: ', receipt)
+      web3.eth
+        .sendSignedTransaction(signedCreateTransaction)
+        .on('transactionHash', function (hash) {
+          console.log('HASH: ', hash)
+        })
+        .on('receipt', function (receipt) {
+          console.log('RECEIPT: ', receipt)
+          web3.eth
+            .call({
+              to: config.alastriaIdentityManager,
+              data: web3.eth.abi.encodeFunctionCall(
+                config.contractsAbi.AlastriaIdentityManager.identityKeys,
+                [entityProviderKeystore.address]
+              )
+            })
+            .then((AlastriaIdentity) => {
+              console.log(
+                `alastriaProxyAddress: 0x${AlastriaIdentity.slice(26)}`
+              )
+              configData.entityIssuer = `0x${AlastriaIdentity.slice(26)}`
+              fs.writeFileSync(
+                '../configuration.json',
+                JSON.stringify(configData)
+              )
+              const alastriaDID = tokensFactory.tokens.createDID(
+                configData.network,
+                AlastriaIdentity.slice(26),
+                configData.networkId
+              )
+              configData.didEntityProvider = alastriaDID
+              fs.writeFileSync(
+                '../configuration.json',
+                JSON.stringify(configData)
+              )
+              console.log('the alastria DID is:', alastriaDID)
+            })
+        })
+
+        .on('error', function (error) {
+          console.error(error)
+          process.exit(1)
+        }) // If a out of gas error, the second parameter is the receipt.
+    })
+
+    .on('error', function (error) {
+      console.error(error)
+      process.exit(1)
+    }) // If a out of gas error, the second parameter is the receipt.
+}
+
+main()
